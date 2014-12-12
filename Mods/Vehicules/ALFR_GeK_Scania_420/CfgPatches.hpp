5b9a14296102288d7f863968b2593aed7c4e2a20
Gekloecht <jerome.everaert@gmail.com>
2014-12-12 16:38:35 +0100

    #ALFR_GeK_Scania_420 - #1.0

---
+++ Mods/Vehicules/ALFR_GeK_Scania_420/CfgPatches.hpp
@@ -0,0 +1,10 @@
+class CfgPatches
+{
+	class ALFR_GeK_Scania_420
+	{
+		units[]				= {"ALFR_GeK_Scania_420"};
+		weapons[]			= {};
+		requiredVersion		= 0.1;
+		requiredAddons[]	= {"A3_Soft_F"};
+	};
+};
